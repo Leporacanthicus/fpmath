6fd4e0caf5f978b24230cff10d5b74ba05bc003f
Mats Petersson <mats@planetcatfish.com>
2015-12-13 18:57:08 +0000

    Add subtraction and FP32 support

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 all: fpmath
 
 CXX=clang++
-CXXFLAGS=-std=c++11 -Wall -Werror
+CXXFLAGS=-std=c++11 -Wall -Werror -g
 
 fpmath: fpmath.cpp
+
+clean:
+	rm -f fpmath
--- fpmath.cpp
+++ fpmath.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <cstdint>
 #include <climits>
+#include <cstring>
 
 template <typename T, int EBITS, int MBITS>
 class Float
 {
 public:
     static const int expBits = EBITS;
-    static const int expOffset = EBITS >> 1;
+    static const int expOffset = (1 << (EBITS-1));
     static const int mantBits = MBITS;
+    static const T mantMask = (1 << mantBits)-1;
     typedef T intType;
     Float(T v)
 	{
 	    val.whole = v;
 	}
-    Float(int s, int e, T m);
+    Float() { val.whole = 0xdeaddead; }
+    Float(const Float& rhs) { val.whole = rhs.val.whole; }
     static_assert((EBITS + MBITS + 1) / CHAR_BIT == sizeof(T),
 		  "Expect bits in T to match EBITS + MBITS + 1");
     union FloatValue
@@ -28,17 +31,20 @@ public:
 	} bits;
     };
 
-    Float operator+=(Float rhs);
+    Float operator+=(const Float& rhs);
+    Float operator-=(const Float& rhs);
 
-    int Exp() { return val.bits.exp - expOffset; }
-    T Mant();
+    int Exp() const { return val.bits.exp - expOffset; }
+    T Mant() const;
+    void Mant(T m) { val.bits.mant = m & mantMask; }
+    void Exp(int e) { val.bits.exp = e + expOffset; }
     
 //private:
     FloatValue val;
 };
 
 template<typename T, int EBITS, int MBITS>
-T Float<T, EBITS, MBITS>::Mant()
+T Float<T, EBITS, MBITS>::Mant() const
 {
     typedef Float<T, EBITS, MBITS> FP;
     if ((val.bits.mant|val.bits.exp) == 0)
@@ -49,27 +55,21 @@ T Float<T, EBITS, MBITS>::Mant()
     {
 	return val.bits.mant | (1 << FP::mantBits);
     }
+    // Denormal.
     return val.bits.mant;
 }
 
 template<typename T, int EBITS, int MBITS>
-Float<T, EBITS, MBITS>::Float(int s, int e, T m)
+Float<T, EBITS, MBITS> Float<T, EBITS, MBITS>::operator+=(const Float<T, EBITS, MBITS>& rhs)
 {
-    val.bits.sign = s;
-    val.bits.exp = e + expOffset;
-    val.bits.mant = m;
-}
-
-template<typename T, int EBITS, int MBITS>
-Float<T, EBITS, MBITS> Float<T, EBITS, MBITS>::operator+=(Float<T, EBITS, MBITS> rhs)
-{
-    int le = Exp();
+    int newExp = Exp();
     int re = rhs.Exp();
     intType lm = Mant();
     intType rm = rhs.Mant();
-    int diff = re - le;
-    if ( diff > 0 )
+    int diff = re - newExp;
+    if (diff > 0)
     {
+	newExp = re;
 	lm >>= diff;
     }
     else if (diff < 0)
@@ -77,21 +77,47 @@ Float<T, EBITS, MBITS> Float<T, EBITS, MBITS>::operator+=(Float<T, EBITS, MBITS>
 	rm >>= -diff;
     }
 
-    int newExp = std::max(le, re);
     intType m = rm + lm;
-    intType mask = (1 << mantBits)-1;
-    while ((m & ~((1 << mantBits)-1)) > (1 << mantBits))
+    while ((m & ~mantMask) > (1 << mantBits))
     {
 	newExp ++;
 	m >>= 1;
     }
-    m &= mask;
-    val.bits.mant = m;
-    val.bits.exp = newExp + expOffset;
-    
+    Mant(m);
+    Exp(newExp);
+
     return *this;
 }
 
+template<typename T, int EBITS, int MBITS>
+Float<T, EBITS, MBITS> Float<T, EBITS, MBITS>::operator-=(const Float<T, EBITS, MBITS>& rhs)
+{
+    int newExp = Exp();
+    int re = rhs.Exp();
+    intType lm = Mant();
+    intType rm = rhs.Mant();
+    int diff = re - newExp;
+    if (diff > 0)
+    {
+	newExp = re;
+	lm >>= diff;
+    }
+    else if (diff < 0)
+    {
+	rm >>= -diff;
+    }
+
+    intType m = lm - rm;
+    while ((m & ~mantMask) < (1 << mantBits))
+    {
+	newExp --;
+	m <<= 1;
+    }
+    Mant(m);
+    Exp(newExp);
+
+    return *this;
+}
 
 template<typename T>
 T operator+(T lhs, T rhs)
@@ -101,7 +127,30 @@ T operator+(T lhs, T rhs)
     return res;
 }
 
+template<typename T>
+T operator-(T lhs, T rhs)
+{
+    T res = lhs;
+    res -= rhs;
+    return res;
+}
+
 using FP16 = Float<uint16_t, 5, 10>;
+using FP32 = Float<uint32_t, 8, 23>;
+
+float FP32ToFloat(FP32 x)
+{
+    float r;
+    memcpy(&r, &x, sizeof(float));
+    return r;
+}
+
+FP32 FloatToFP32(float x)
+{
+    FP32 r;
+    memcpy(&r, &x, sizeof(float));
+    return r;
+}
 
 int main()
 {
@@ -117,7 +166,15 @@ int main()
     std::cout << std::hex << c.val.whole << std::endl;
     c += a;
     std::cout << std::hex << c.val.whole << std::endl;
+
+    FP32 d(FloatToFP32(0.77f));
+    FP32 e(FloatToFP32(1.82f));
+
+    FP32 f = d + e;
+    std::cout << std::hex << f.val.whole << std::endl;
+
+    std::cout << FP32ToFloat(f) << std::endl;
+
+    f -= e;
+    std::cout << FP32ToFloat(f) << std::endl;
 }
-
-
-
